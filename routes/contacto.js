6e7b2672b2f83b5a7b45480515fc4905fe235080
Melina Joloidovsky B <melina.jb@hotmail.com>
2021-12-14 22:16:51 -0300

    conexion a mailtrap

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//muestra la vista
 router.get('/', function(req,res,next){
-    res.render('contacto')//contacto.hbs
+    res.render('contacto', { 
+        isContacto: true
+       });//contacto.hbs
 })
 
+//funcionamiento del formulario
+router.post('/', async function(req, res, next) {
 
+});
 module.exports=router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Batatini' });
+  res.render('index', { 
+    isHome:true
+   });
 });//res render me muestra el archivo hbs q es index
 
 module.exports = router;
